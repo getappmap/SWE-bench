7175566d7993714ced74d644145318bada6b1a35
Rafał Rzepecki <divided.mind@gmail.com>
2024-05-07 13:06:57 +0200

    Set correct PYTHONPATH in run_evaluations.sh

--- swebench/harness/run_evaluation.sh
+++ swebench/harness/run_evaluation.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
-python run_evaluation.py \
-    --predictions_path "<path to predictions (.json)>" \
-    --swe_bench_tasks "<path to `swe-bench.json`>" \
-    --log_dir "<path to folder>" \
-    --testbed "<path to folder>" \
+
+# find project root directory
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+# add it to python path
+export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
+
+# get predictions path from the first argument
+# (error if not provided)
+if [ -z "$1" ]; then
+    echo "Error: predictions path not provided"
+    exit 1
+fi
+PREDICTIONS="$1"
+
+# get SWE-Bench tasks from the second argument
+# (error if not provided)
+if [ -z "$2" ]; then
+    echo "Error: SWE-Bench tasks not provided"
+    exit 1
+fi
+TASKS="$2"
+
+LOG_DIR="$PROJECT_ROOT/eval_logs/"
+
+python $PROJECT_ROOT/swebench/harness/run_evaluation.py \
+    --predictions_path "$PREDICTIONS" \
+    --swe_bench_tasks "$TASKS" \
+    --log_dir "$LOG_DIR" \
+    --testbed /tmp \
     --skip_existing \
     --timeout 900 \
-    --verbose
+    --verbose \
+    --num_processes 1 \
+    --path_conda /home/divide/opt/miniconda3/
