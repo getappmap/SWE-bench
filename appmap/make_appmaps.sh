e33c44259ad1e9fa6d7164ad13a2f92373db2e2b
Rafał Rzepecki <divided.mind@gmail.com>
2024-05-23 18:13:06 +0200

    default dataset (lite) for make_appmaps

--- appmap/make_appmaps.sh
+++ appmap/make_appmaps.sh
@@ -5,16 +5,14 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 # add it to python path
 export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
 
-if [ -z "$1" ]; then
-    echo "Usage: $0 <instances_path_or_name>"
-    exit 1
-fi
+INSTANCES="princeton-nlp/SWE-bench_Lite"
 
 python $PROJECT_ROOT/appmap/make_appmaps.py \
-    --instances_path "$1" \
+    --instances_path "$INSTANCES" \
     --log_dir "appmap_logs" \
     --appmap_archive_dir appmaps \
     --temp_dir "/tmp/swe-appmaps" \
     --num_workers 1 \
     --verbose \
-    --path_conda $(conda info --base)
+    --path_conda $(conda info --base) \
+    "$@"
