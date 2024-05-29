806309d13b262201d7939a9c8f2163225bb243bc
Rafał Rzepecki <divided.mind@gmail.com>
2024-05-29 08:13:29 +0000

    Small convenience script updates

--- swebench/harness/run_evaluation.sh
+++ swebench/harness/run_evaluation.sh
@@ -5,31 +5,31 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 # add it to python path
 export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
 
-# get predictions path from the first argument
-# (error if not provided)
-if [ -z "$1" ]; then
-    echo "Error: predictions path not provided"
+SCENARIO="$1"
+SCENARIO_DIR=/experiments/experiments/evaluation/lite/$SCENARIO
+
+PREDICTIONS=$SCENARIO_DIR/all_preds.jsonl
+if ! [ -f "$PREDICTIONS" ]; then
+    echo "Error: predictions path $PREDICTIONS not found"
     exit 1
 fi
-PREDICTIONS="$1"
 
-# get SWE-Bench tasks from the second argument
-# (error if not provided)
-if [ -z "$2" ]; then
+TASKS="princeton-nlp/SWE-bench_Lite"
+if [ -z "$TASKS" ]; then
     echo "Error: SWE-Bench tasks not provided"
     exit 1
 fi
-TASKS="$2"
 
-LOG_DIR="$PROJECT_ROOT/eval_logs/"
+LOG_DIR=$SCENARIO_DIR/logs
 
 python $PROJECT_ROOT/swebench/harness/run_evaluation.py \
     --predictions_path "$PREDICTIONS" \
     --swe_bench_tasks "$TASKS" \
     --log_dir "$LOG_DIR" \
-    --testbed /tmp \
+    --flat \
+    --testbed /mnt \
     --skip_existing \
     --timeout 900 \
     --verbose \
-    --num_processes 1 \
-    --path_conda /home/divide/opt/miniconda3/
+    --num_processes 8 \
+    --path_conda $(conda info --base)
