b111bae81902d18e91cba622e3c0a4e85218c979
Pete Cheslock <petecheslock@gmail.com>
2024-06-03 14:10:33 -0400

    Basic test to see if enviornment is working

---
+++ appmap/make_appmaps-github-actions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# find project root directory
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+# add it to python path
+export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
+
+INSTANCES="princeton-nlp/SWE-bench_Lite"
+
+python $PROJECT_ROOT/appmap/make_appmaps.py \
+    --instances_path "$INSTANCES" \
+    --log_dir "appmap_logs" \
+    --appmap_archive_dir appmaps \
+    --temp_dir "/tmp/swe-appmaps" \
+    --num_workers 1 \
+    --verbose \
+    --path_conda $(conda info --base) \
+    --appmap-bin ./submodules/appmap-js/packages/cli/built/cli.js \
+    "$@"
