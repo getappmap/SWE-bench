0a4fcf03f3d9356cf4d2df64f2a38a009a346ade
Kevin Gilpin <kgilpin@gmail.com>
2024-06-04 10:35:31 -0400

    feat: Dedicated log files for lint and diff

---
+++ appmap/solve/log.py
@@ -0,0 +1,21 @@
+import os
+
+from filelock import FileLock
+
+
+def log_lint(dir, file, lint_messages):
+    lint_lock_file = os.path.join(dir, "lint.lock")
+    lint_log_file = os.path.join(dir, "lint.log")
+
+    with FileLock(lint_lock_file):
+        with open(lint_log_file, "a+") as f:
+            f.writelines("\n".join([file, "-" * len(file), lint_messages, "\n"]))
+
+
+def log_diff(dir, file, diff):
+    diff_lock_file = os.path.join(dir, "diff.lock")
+    diff_log_file = os.path.join(dir, "diff.log")
+
+    with FileLock(diff_lock_file):
+        with open(diff_log_file, "a+") as f:
+            f.writelines("\n".join([file, "-" * len(file), diff, "\n"]))
