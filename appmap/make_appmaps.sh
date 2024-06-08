7b9ce3cb5c2890a4da36f0ed864f239c135bbdc0
Alan Potter <alanjpotter@gmail.com>
2024-06-08 13:16:49 -0400

    fix: allow appmap-js directory to be overridden

--- appmap/make_appmaps.sh
+++ appmap/make_appmaps.sh
@@ -15,5 +15,5 @@ python $PROJECT_ROOT/appmap/make_appmaps.py \
     --num_workers 1 \
     --verbose \
     --path_conda $(conda info --base) \
-    --appmap-bin /home/swe-bench/appmap-js/packages/cli/built/cli.js \
+    --appmap_bin  "${APPMAP_JS:-/home/swe-bench/appmap-js}/packages/cli/built/cli.js \
     "$@"
