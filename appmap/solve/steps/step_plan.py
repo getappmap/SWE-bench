def8fa4ddb5e36fe058c09fb5130d5df546fd4c0
Kevin Gilpin <kgilpin@gmail.com>
2024-06-13 11:51:02 -0400

    feat: Generate custom keywords and send to the context function

--- appmap/solve/steps/step_plan.py
+++ appmap/solve/steps/step_plan.py
@@ -21,13 +21,44 @@ def step_plan(
     with open(issue_file, "r") as f:
         issue_content = f.read()
 
+    print(f"[plan] ({instance_id}) Rewriting the issue as code search keywords")
+    terms_question = os.path.join(work_dir, "search_terms.txt")
+    terms_output = os.path.join(work_dir, "search_terms.json")
+    terms_log = os.path.join(work_dir, "search_terms.log")
+    with open(terms_question, "w") as rewrite_f:
+        rewrite_f.write(
+            f"""@generate /nocontext
+
+
+Generate a list of all file names, module names, class names, function names and varable names that are mentioned in the
+described issue. Do not emit symbols that are part of the programming language itself. Do not emit symbols that are part
+of test frameworks. Focus on library and application code only. Emit the results as a JSON list. Do not emit text, markdown, 
+or explanations.
+
+<issue>
+{issue_content}
+</issue>
+"""
+        )
+    run_navie_command(
+        log_dir,
+        temperature=temperature,
+        command=appmap_command,
+        input_path=terms_question,
+        output_path=terms_output,
+        log_path=terms_log
+    )
+
+    with open(terms_output, "r") as f:
+        issue_content_as_code = f.read()
+
     print(f"[plan] ({instance_id}) Searching for context using {issue_file}")
     context_prompt = os.path.join(work_dir, "search_context.txt")
     with open(context_prompt, "w") as apply_f:
         apply_f.write(
             f"""@context /nofence /format=json /noterms /exclude=(\\btesting\\b|\\btest\\b|\\btests\\b|\\btest_|_test\.py$|\.txt$|\.html$|\.rst$|\.md$)
                       
-{issue_content}
+{issue_content_as_code}
 """
         )
 
@@ -70,6 +101,7 @@ DO NOT choose a test case file.
         command=appmap_command,
         input_path=plan_question,
         prompt_path=plan_prompt,
+        context_path=context_file,
         output_path=plan_file,
         log_path=os.path.join(work_dir, "plan.log"),
     )
