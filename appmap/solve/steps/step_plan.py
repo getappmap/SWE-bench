713825062151b17add7db35dfed5f412d8ef8b56
Kevin Gilpin <kgilpin@gmail.com>
2024-06-14 15:32:22 -0400

    feat: Allow multi-file edits

--- appmap/solve/steps/step_plan.py
+++ appmap/solve/steps/step_plan.py
@@ -88,12 +88,10 @@ or explanations.
     with open(plan_prompt, "w") as plan_f:
         plan_f.write(
             """
-Focus the plan on modifying exactly one file.
-
 Do not modify test case files. Test case files are those that include "test", "tests" in their paths,
 or match the patterns "*_test.py" or "test_*.py".
 
-DO choose the one most relevant file to modify.
+DO choose the most relevant files to modify.
 DO NOT modify any other files.
 DO NOT choose a test case file.
 """
